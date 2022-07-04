c815df074f2cd06f3aa9f981f25e4e6d257d9729
ariesclark <me@ariesclark.com>
2022-07-04 00:54:12 -0600

    remove extra packages + clean code

--- apps/api/src/pages/api/schedule.ts
+++
@@ -1,7 +0,0 @@
-import { getSchedule } from "../../utils/subsplease";
-
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-	res.json(await getSchedule());
-};
