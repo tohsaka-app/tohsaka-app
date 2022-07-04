e79450185c0fff5f6b4a8ec8993cabfa857db989
ariesclark <me@ariesclark.com>
2022-07-04 01:11:16 -0600

    don't generate graphql files on build

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+	rewrites: async () => ({
+		afterFiles: [
+			{
+				source: "/:path*",
+				destination: "/api/:path*"
+			}
+		]
+	})
 };
 
 module.exports = nextConfig;
